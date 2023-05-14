2e78311f660ebae90e393958b2be32206d15b868
Chika Kalu Ukpaii <chikau@phreetech.com>
2023-05-14 18:02:09 +0100

    tutorial #15

--- src/app/user-auth/login/login.component.ts
+++ src/app/user-auth/login/login.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   login(name: string) {
       alert(`You are now logged in as ${name}`)
-    }
+  }
+  
+  age = 32;
+  male = false;
 }
